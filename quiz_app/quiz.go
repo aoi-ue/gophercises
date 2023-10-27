7d5c7cad3b0890dafb06f537cedbfdb4e528108f
yuhanyuhan <yuhanlian@gmail.com>
2023-10-27 14:02:53 +0800

    implemented: timer flag

--- quiz_app/quiz.go
+++ quiz_app/quiz.go
@@ -8,10 +8,11 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
-	filePath := initFile()
+	filePath, timeLimit := initFile()
 
 	// "read in a quiz provided via a CSV file"
 	file, err := os.Open(filePath)
@@ -52,19 +53,36 @@ func main() {
 	score := 0
 	scanner := bufio.NewScanner(os.Stdin)
 
-	for i, question := range questions {
-		fmt.Printf("Question %d: %s\nYour answer: ", i+1, question)
+	// create a ticker for the time limit and a channel to signal the user finished the quiz
+	ticker := time.NewTicker(time.Second * time.Duration(timeLimit))
+	done := make(chan bool)
 
-		scanner.Scan()
-		userAnswer := scanner.Text()
-		userAnswer = strings.TrimSpace(userAnswer)
+	// Answer questions
+	go func() {
+		for i, question := range questions {
+			fmt.Printf("Question %d: %s\nYour answer: ", i+1, question)
 
-		if userAnswer == answers[i] {
-			fmt.Println("Correct!")
-			score++
-		} else {
-			fmt.Println("Incorrect. The correct answer is:", answers[i])
+			scanner.Scan()
+			userAnswer := scanner.Text()
+			userAnswer = strings.TrimSpace(userAnswer)
+
+			if userAnswer == answers[i] {
+				fmt.Println("Correct!")
+				score++
+			} else {
+				fmt.Println("Incorrect. The correct answer is:", answers[i])
+			}
 		}
+		done <- true
+	}()
+
+	// select chooses the first channel with an available value
+	// if done is available first, the user finished
+	// if ticker is available first, the time limit has been reached
+	select {
+	case <-done:
+	case <-ticker.C:
+		fmt.Println("time's up!")
 	}
 
 	//  output the total number of questions correct and how many questions there were in total.
@@ -72,9 +90,10 @@ func main() {
 }
 
 // default to problems.csv (example shown below), but the user should be able to customize the filename via a flag.
-func initFile() string {
+// default time limit should be 30 seconds, but should also be customizable via a flag.
+func initFile() (string, int) {
 	filePath := flag.String("file", "problems.csv", "Path to the CSV file")
-
+	timeLimit := flag.Int("timer", 30, "Time Limit is 30sec by default")
 	flag.Parse()
 
 	if *filePath == "" {
@@ -83,7 +102,7 @@ func initFile() string {
 
 	fmt.Printf("Reading file: %s\n", *filePath)
 
-	return *filePath
+	return *filePath, *timeLimit
 }
 
 func Add(a, b int) int {
