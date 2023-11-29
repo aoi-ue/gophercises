18dd295506c767ee76d31df2c06e8a4d2391fc55
yuhanyuhan <yuhanlian@gmail.com>
2023-11-29 17:50:59 +0800

    implement: set up base code using bubbletea

--- quiz_app/quiz.go
+++ quiz_app/quiz.go
@@ -1,118 +1,90 @@
 package main
 
 import (
-	"bufio"
-	"encoding/csv"
-	"flag"
 	"fmt"
 	"log"
 	"os"
-	"strings"
-	"time"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
+// main func
 func main() {
-	filePath, timeLimit := initFile()
+	p := tea.NewProgram(initialModel())
 
-	// "read in a quiz provided via a CSV file"
-	file, err := os.Open(filePath)
-	if err != nil {
+	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
+		os.Exit(1)
 	}
 
-	defer file.Close()
+}
 
-	// read line by line and field by field
-	reader := csv.NewReader(file)
+// model as state: question to prompt, correctcount to keep track, timer to end
+type model struct {
+	textInput textinput.Model
+	// questions []question
+	correctcount int
+	timer        int
+	err          error
+}
 
-	questions := make([]string, 0)
-	answers := make([]string, 0)
+// type question struct {
+// 	Question string
+// 	Answer string
+// }
 
-	for {
-		record, err := reader.Read()
+// initial state with bubbletea: to read textinput,
+func initialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "Enter Answer Here"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 20
 
-		if err != nil {
-			// Check for end of file
-			if err.Error() == "EOF" {
-				break
-			} else {
-				fmt.Println("Error reading record:", err)
-				return
-			}
+	return model{
+		textInput: ti,
+		// questions []question
+		correctcount: 0,
+		// todo: have to take timer from cobra's flag
+		timer: 30,
+		err:   nil,
+	}
+}
+
+// init actionable: blink and display
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+// update: update the correctcount, readline to trigger view render
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "esc":
+			return m, tea.Quit
+		case "enter", " ":
+			// todo: evaluate if input is same as column[1]
+			return m, textinput.Blink
 		}
-
-		if len(record) == 2 {
-			questions = append(questions, record[0])
-			answers = append(answers, record[1])
-		} else {
-			continue
-		}
 	}
 
-	// keeping track of how many questions they get right and how many they get incorrect.
-	score := 0
-	scanner := bufio.NewScanner(os.Stdin)
+	return m, cmd
 
-	// create a ticker for the time limit and a channel to signal the user finished the quiz
-	ticker := time.NewTicker(time.Second * time.Duration(timeLimit))
-	done := make(chan bool)
-
-	WaitForStart()
-
-	// Answer questions
-	go func() {
-		for i, question := range questions {
-			fmt.Printf("Question %d: %s\nYour answer: ", i+1, question)
-
-			scanner.Scan()
-			userAnswer := scanner.Text()
-			userAnswer = strings.TrimSpace(userAnswer)
-
-			if userAnswer == answers[i] {
-				fmt.Println("Correct!")
-				score++
-			} else {
-				fmt.Println("Incorrect. The correct answer is:", answers[i])
-			}
-		}
-		done <- true
-	}()
-
-	// select chooses the first channel with an available value
-	// if done is available first, the user finished
-	// if ticker is available first, the time limit has been reached
-	select {
-	case <-done:
-	case <-ticker.C:
-		fmt.Println("time's up!")
-	}
-
-	//  output the total number of questions correct and how many questions there were in total.
-	fmt.Printf("Quiz completed! Your score: %d/%d\n", score, len(questions))
 }
 
-// default to problems.csv (example shown below), but the user should be able to customize the filename via a flag.
-// default time limit should be 30 seconds, but should also be customizable via a flag.
-func initFile() (string, int) {
-	filePath := flag.String("file", "problems.csv", "Path to the CSV file")
-	timeLimit := flag.Int("timer", 30, "Time Limit is 30sec by default")
-	flag.Parse()
-
-	if *filePath == "" {
-		fmt.Println("If problems.csv is not created, please provide a file path using the -file flag.")
-	}
-
-	fmt.Printf("Reading file: %s\n", *filePath)
-
-	return *filePath, *timeLimit
+// view: simple view of a quiz, render everytime for new readLine
+func (m model) View() string {
+	return fmt.Sprintf(
+		"Question Goes here\n\n%s\n\n%s",
+		m.textInput.View(),
+		"(ctrl c or esc to quit)",
+	)
 }
 
-// WaitForStart makes the program wait for a user to press Enter button
-func WaitForStart() {
-	fmt.Print("Press 'Enter' to start the quiz.")
-	bufio.NewReader(os.Stdin).ReadBytes('\n')
-}
+// lipgloss: for styling
 
-func Add(a, b int) int {
-	return a + b
-}
+// readCSV: purely to read CSV
