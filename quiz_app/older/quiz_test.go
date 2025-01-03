eedfd3eb3a8fab02a811485ee46963035943aff0
yuhanyuhan <yuhanlian@gmail.com>
2025-01-03 16:04:24 +0800

    testcase: included func

--- quiz_app/older/quiz_test.go
+++ quiz_app/older/quiz_test.go
@@ -3,7 +3,7 @@ package main
 import (
 	"testing"
 
-	"gotest.tools/assert"
+	assert "gotest.tools/assert"
 )
 
 // template for running test
@@ -12,8 +12,16 @@ func TestAdd(t *testing.T) {
 	expected := 3
 
 	if sum != expected {
-		t.Errorf("expected '%d' but got '%d'", expected, sum)
+		t.Errorf("expected '%d' but got '%d'", expected, sum) 
 	}
 
 	assert.Equal(t, sum, expected)
+}
+
+func runCommand(cmd string) ([]byte, error) {
+	return nil, nil 
+}
+
+func TestQuizProgram(t *testing.T) {
+	return 
 }
